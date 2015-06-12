01a4f004a84d22a62cdddf47a817d11b710dac58
hitman07 <david.malosse@voila.fr>
2015-06-12 14:08:34 +0200

    evolution lien avec page du formulaire de saisie command

--- app/models/command.rb
+++ app/models/command.rb
@@ -1,2 +1,10 @@
 class Command < ActiveRecord::Base
+	validates :numclient, :presence => true
+	validates :refproduit, :presence => true,
+                    :length => { :minimum => 4 }
+    validates :quantitearticle, :presence => true,
+                    :length => { :minimum => 1 }
+
 end
+#  classe Command hérite de ActiveRecord::Base. Active Record fournit une grande partie des fonctionnalités de votre modèle Rails,
+# notamment les opérations CRUD – Create, Read, Update, Destroy (Créer, Lire, Modifier, Détruire)- de base, la validation des données, ainsi que le support d’une recherche sophistiquée et la possibilité de lier des modèles entre eux.
